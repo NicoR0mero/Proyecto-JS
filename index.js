b51c57fda61b3bcf5355681d53b0b6747902a817
NicoR0mero <nr064056@gmail.com>
2025-09-08 20:45:43 -0300

    Termino actividad

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-let bienvenida = "Bienvenidos A COTO DIGITAL";
-console.log(bienvenida);
+alert("Bienvenidos A COTO DIGITAL");
 
-alert(bienvenida);
-
-loguear();
-
-//------------------ CONSTANTES PARA VALIDACION DE USUARIO----------- //
+//--------- CONSTANTES ----------- //
 const Usuario1 = "Nico";
 const Contraseña1 = "1234";
+
+//--------- VARIABLES GLOBALES ----------- //
+let carrito = [];
+let total = 0;
 
 //----------------------------- FUNCIONES -------------------------------------//
 function loguear() {
@@ -15,28 +14,78 @@ function loguear() {
   let contraseña = prompt("Ingrese contraseña");
   let mensaje = "";
 
-  if (usuario === "Nico" && contraseña === "1234") {
+  if (usuario === Usuario1 && contraseña === Contraseña1) {
     mensaje = "Acceso concedido. ¡Bienvenido!";
-    MostrarStock();
+    alert(mensaje);
+    Armaelcarrito();
   } else if (usuario === "" || contraseña === "") {
     mensaje = "Los campos no pueden estar vacíos.";
+    alert(mensaje);
+    loguear();
   } else {
     mensaje = "Usuario o contraseña incorrectos.";
-  }
-
-  alert(mensaje);
-}
-
-//------------------------ Clase de array --------------------//
-class Productos {
-  constructor(id, precio) {
-    this.id = id;
-    this.precio = precio;
+    alert(mensaje);
+    loguear();
   }
 }
 
-let nuevos_productos = new Array();
-nuevos_productos.push(new Productos("Pan", 4000));
-nuevos_productos.push(new Productos("Leche", 2500));
-nuevos_productos.push(new Productos("Aceite", 3500));
-nuevos_productos.push(new Productos("Fideos", 760));
+function Armandopedido(opcion) {
+  switch (opcion) {
+    case "1":
+      carrito.push({ nombre: "Banana", precio: 1000 });
+      total += 1000;
+      break;
+    case "2":
+      carrito.push({ nombre: "Fideos", precio: 500 });
+      total += 500;
+      break;
+    case "3":
+      carrito.push({ nombre: "Arroz", precio: 3000 });
+      total += 3000;
+      break;
+    case "4":
+      carrito.push({ nombre: "Gaseosa", precio: 4500 });
+      total += 4500;
+      break;
+    case "5":
+      carrito.push({ nombre: "Leche", precio: 1500 });
+      total += 1500;
+      break;
+  }
+}
+
+function totalcuenta() {
+  let resumen = " Tu pedido:\n";
+  carrito.forEach((producto, valor) => {
+    resumen += `${valor + 1}. ${producto.nombre} - $${producto.precio}\n`;
+  });
+  resumen += `\n Total a pagar: $${total}`;
+  alert(resumen);
+}
+
+function Armaelcarrito() {
+  const lista = prompt(
+    "Seleccione los productos para su lista de pedidos \n 1-Banana $1000 \n 2-Fideos $500 \n 3-Arroz $3000 \n 4-Gaseosa $4500 \n 5-Leche $1500 \n 6-Terminar pedido."
+  );
+
+  switch (lista) {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+      Armandopedido(lista);
+      alert("Producto agregado al carrito.");
+      Armaelcarrito();
+      break;
+    case "6":
+      totalcuenta();
+      break;
+    default:
+      alert("Opción inválida.");
+      Armaelcarrito();
+  }
+}
+
+// Ejecutar login
+loguear();
